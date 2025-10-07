db7c764df8474d47f651a3a8c92256a747d5668c
ChrisBGH <Chrisbouetayy@gmail.com>
2025-10-07 14:49:38 +0200

    test123

--- CIT12Assignment3/Models/CategoryService.cs
+++ CIT12Assignment3/Models/CategoryService.cs
@@ -2,40 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 
-    public class Category
-    {
-        public int Cid { get; set; }
-        public string Name { get; set; }
-    }
 
-    public class CategoryService
-    {
-        private readonly Dictionary<int, Category> _categories = new()
-    {
-        { 1, new Category { Cid = 1, Name = "Beverages" } },
-        { 2, new Category { Cid = 2, Name = "Condiments" } },
-        { 3, new Category { Cid = 3, Name = "Confections" } }
-    };
+   public class Category
+   {
+       public int Cid { get; set; }
+       public string Name { get; set; }
+   }
 
-        public List<Category> GetCategories() => _categories.Values.ToList();
 
-        public Category? GetCategory(int cid) =>
-            _categories.TryGetValue(cid, out var category) ? category : null;
+   public class CategoryService
+   {
+       private readonly Dictionary<int, Category> _categories = new()
+   {
+       { 1, new Category { Cid = 1, Name = "Beverages" } },
+       { 2, new Category { Cid = 2, Name = "Condiments" } },
+       { 3, new Category { Cid = 3, Name = "Confections" } }
+   };
 
-        public bool UpdateCategory(int id, string newName)
-        {
-            if (!_categories.ContainsKey(id)) return false;
-            _categories[id].Name = newName;
-            return true;
-        }
 
-        public bool DeleteCategory(int id) => _categories.Remove(id);
+       public List<Category> GetCategories() => _categories.Values.ToList();
 
-        public bool CreateCategory(int id, string name)
-        {
-            if (_categories.ContainsKey(id)) return false;
-            _categories[id] = new Category { Cid = id, Name = name };
-            return true;
-        }
-    }
 
+       public Category? GetCategory(int cid) =>
+           _categories.TryGetValue(cid, out var category) ? category : null;
+
+
+       public bool UpdateCategory(int id, string newName)
+       {
+           if (!_categories.ContainsKey(id)) return false;
+           _categories[id].Name = newName;
+           return true;
+       }
+
+
+       public bool DeleteCategory(int id) => _categories.Remove(id);
+
+
+       public bool CreateCategory(int id, string name)
+       {
+           if (_categories.ContainsKey(id)) return false;
+           _categories[id] = new Category { Cid = id, Name = name };
+           return true;
+       }
+   }
